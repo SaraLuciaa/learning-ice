3ba1f961d156022527ece3ab69c82f0e1499e7d1
Sara Lucia <saludipu71@gmail.com>
2025-05-26 15:18:45 -0500

    delete comments

--- server/src/main/java/Server.java
+++ server/src/main/java/Server.java
@@ -7,26 +7,6 @@ public class Server
         {
             Demo.AckServicePrx ack = Demo.AckServicePrx.uncheckedCast( 
                 communicator.stringToProxy("AckService:default -h localhost -p 10010"));;
-            // int retries = 5;
-
-            // while (ack == null && retries-- > 0) {
-            //     try {
-            //         ack = Demo.AckServicePrx.checkedCast( 
-            //             communicator.stringToProxy("AckService:default -h localhost -p 10010"));
-            //     } catch (com.zeroc.Ice.ObjectNotExistException e) {
-            //         System.out.println("❌ AckService no disponible aún. Reintentando...");
-            //         try {
-            //             Thread.sleep(10000);
-            //         } catch (InterruptedException e2) {
-            //             System.err.println("Error al esperar: " + e2.getMessage());
-            //             return;
-            //         }
-            //     }
-            // }
-            // if (ack == null) {
-            //     System.out.println("🚨 No se pudo conectar a AckService. Abortando.");
-            //     return;
-            // }
 
             com.zeroc.Ice.ObjectAdapter adapter = communicator.createObjectAdapterWithEndpoints("PrinterAdapter", "default -p 10011");
             com.zeroc.Ice.Object object = new PrinterI(ack);
